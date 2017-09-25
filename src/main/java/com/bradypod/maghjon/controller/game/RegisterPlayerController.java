335f6c46404e8f426549960b5939be6f88f652be
14768776836 <784700109@qq.com>
2017-09-25 14:24:43 +0800

    项目分目录

---
+++ src/main/java/com/bradypod/maghjon/controller/game/RegisterPlayerController.java
@@ -0,0 +1,7 @@
+package com.bradypod.maghjon.controller.game;
+
+/**
+ * Created by Administrator on 2017/9/25/025.
+ */
+public class RegisterPlayerController {
+}
---
+++ src/main/java/com/bradypod/maghjon/controller/transact/RoomCardController.java
@@ -0,0 +1,7 @@
+package com.bradypod.maghjon.controller.transact;
+
+/**
+ * Created by Administrator on 2017/9/25/025.
+ */
+public class RoomCardController {
+}
