afa0c0640990aec39ca2c70edec64afcd7df544f
junchaoyu <32057790+junchaoyu@users.noreply.github.com>
2017-09-25 16:29:45 +0800

    余俊超 微信登陆注册

--- src/main/java/com/bradypod/web/handler/mobileter/game/RegisterPlayerController.java
+++ src/main/java/com/bradypod/web/handler/mobileter/game/RegisterPlayerController.java
@@ -1,7 +1,97 @@
 package com.bradypod.web.handler.mobileter.game;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.bradypod.web.model.PlayUser;
+import com.bradypod.web.service.repository.jpa.PlayUserRepository;
+import com.google.gson.Gson;
+
 /**
- * Created by Administrator on 2017/9/25/025.
+ * @ClassName: RegisterPlayerController
+ * @Description: TODO(注册玩家控制层)
+ * @author dave
+ * @date 2017年9月25日 下午4:03:15
  */
+@Controller
+@RequestMapping("/registerPlayer")
 public class RegisterPlayerController {
+
+	@Autowired
+	private PlayUserRepository playUserRes;
+
+	@ResponseBody
+	@RequestMapping("/wxLogin")
+	public String wxLogin(String code, HttpSession session) {
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		/** 请求结果 */
+		String result = "{\"subscribe\": 1,\"openid\":\"o6_bmjrPTlm6_2sgVt7hMZOPfL2M\",\"nickname\": \"Band\",\"sex\": 1,\"language\": \"zh_CN\",\"city\":\"广州\",\"province\": \"广东\",\"country\":\"中国\",\"headimgurl\":\"\",\"subscribe_time\": 1382694957,\"unionid\": \"o6_bmasdasdsad6_2sgVt7hMZOPfL\",\"remark\": \"\",\"groupid\":0,  \"tagid_list\":[128,2]}";
+		// String result = WxUserInfo.getWxUserInfo(code);//根据code获取微信用户信息
+		JSONObject jsonObject = (JSONObject) JSON.parse(result);
+		Gson gson = new Gson();
+		try {
+			if (null != jsonObject.get("openid")) {
+				PlayUser playUser = gson.fromJson(result, PlayUser.class);
+				PlayUser newPlayUser = playUserRes.findByOpenid(playUser.getOpenid());
+				if (null == newPlayUser) {
+					SimpleDateFormat formatter = new SimpleDateFormat("yyyyHHmmssSSS");
+					String time = formatter.format(new Date());
+					playUser.setInvitationcode(time);
+					playUserRes.saveAndFlush(playUser);
+					session.setAttribute("playUser", playUser);
+					dataMap.put("playUser", playUser);
+				} else {
+					session.setAttribute("playUser", newPlayUser);
+					dataMap.put("playUser", newPlayUser);
+				}
+				dataMap.put("success", true);
+			} else {
+				dataMap.put("success", false);
+				dataMap.put("msg", "授权失败");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			dataMap.put("success", false);
+			dataMap.put("msg", "授权失败");
+		}
+		return gson.toJson(dataMap);
+	}
+
+	/**
+	 * 方法描述: 微信绑定平台的用户手机号<br>
+	 * 作者：田帅 <br>
+	 * 创建时间：2017-09-16 <br>
+	 * 版本：V1.0
+	 */
+	@RequestMapping("/wxBound")
+	@ResponseBody
+	public Object wxBound(String mobile, String openid, String nickname, String headimgurl) {
+		Map<String, Object> json = new HashMap<String, Object>();
+		System.out.println("mobile=" + mobile);
+		System.out.println("openid=" + openid);
+		System.out.println("nickname=" + nickname);
+		System.out.println("headimgurl=" + headimgurl);
+		if (mobile == null || mobile.isEmpty() || openid == null || openid.isEmpty()) {
+			json.put("status", "203");
+			json.put("message", "参数不完整");
+			return json;
+		}
+		// 赋值书手机号
+		/* 根据手机号查询账号是否存在 */
+		// 该手机号还没有注册
+		// 该手机号已注册
+		// 先查询该手机号是否已绑定微信openid
+		return json;
+	}
 }
--- src/main/java/com/bradypod/web/service/repository/jpa/PlayUserRepository.java
+++ src/main/java/com/bradypod/web/service/repository/jpa/PlayUserRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bradypod.web.model.PlayUser;
 
-public abstract interface PlayUserRepository extends JpaRepository<PlayUser, String>
-{
+public abstract interface PlayUserRepository extends JpaRepository<PlayUser, String> {
+
 	public abstract PlayUser findById(String paramString);
 
 	public abstract PlayUser findByUsername(String username);
@@ -16,7 +16,15 @@ public abstract interface PlayUserRepository extends JpaRepository<PlayUser, Str
 
 	public abstract PlayUser findByUsernameAndPassword(String username, String password);
 
-	public abstract Page<PlayUser> findByDatastatus(boolean datastatus , String orgi, Pageable paramPageable);
+	public abstract Page<PlayUser> findByDatastatus(boolean datastatus, String orgi, Pageable paramPageable);
 
-	public abstract Page<PlayUser> findByDatastatusAndUsername(boolean datastatus , String orgi ,String username ,Pageable paramPageable);
+	public abstract Page<PlayUser> findByDatastatusAndUsername(boolean datastatus, String orgi, String username, Pageable paramPageable);
+
+	/**
+	 * @Title: findByOpenid
+	 * @Description: TODO(通过openid查询用户信息)
+	 * @param openid
+	 * @return 设定文件 PlayUser 返回类型
+	 */
+	public abstract PlayUser findByOpenid(String openid);
 }
