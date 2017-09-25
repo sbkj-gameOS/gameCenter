dc5d731d1ceafd947a14e5910abadd5297f0fbbc
14768776836 <784700109@qq.com>
2017-09-25 20:31:58 +0800

    数据对接

--- src/main/java/com/bradypod/web/handler/mobileter/transact/RoomCardController.java
+++ src/main/java/com/bradypod/web/handler/mobileter/transact/RoomCardController.java
@@ -1,15 +1,21 @@
 package com.bradypod.web.handler.mobileter.transact;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.bradypod.web.handler.Handler;
 import com.bradypod.web.service.repository.jpa.RoomRechargeRecordRepository;
 import com.bradypod.web.service.repository.jpa.RoomTouseRecordRepository;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 房卡记录
@@ -30,8 +36,19 @@ public class RoomCardController extends Handler {
      */
     @RequestMapping({"/rechargeRecord"})
     public ModelAndView rechargeRecord(ModelMap map , HttpServletRequest request){
-        map.addAttribute("RoomRechargeRecord",roomRechargeRecordRepository.findByUserNameAndInvitationCode(null,null,null));
+        map.addAttribute("data",roomRechargeRecordRepository.findByUserNameAndInvitationCode(null,null));
         return request(super.createAppsTempletResponse("/apps/business/platform/room/recharge/index"));
+    }
+
+    @RequestMapping({"/getRechargeJson"})
+    @ResponseBody
+    public JSONObject getRecharge(){
+        Map<Object,Object> dataMap = new HashMap<Object,Object>();
+        dataMap.put("data",roomRechargeRecordRepository.findByUserNameAndInvitationCode("",""));
+        dataMap.put("count",2);
+        dataMap.put("code",0);
+        dataMap.put("msg","");
+        return (JSONObject) JSONObject.toJSON(dataMap);
     }
 
     /**
