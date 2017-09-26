32963ae76f51a6ce52a8e9ef5a36425037ff1df9
14768776836 <784700109@qq.com>
2017-09-26 20:13:23 +0800

    微信pay

--- src/main/java/com/bradypod/web/handler/mobileter/wx/WxController.java
+++ src/main/java/com/bradypod/web/handler/mobileter/wx/WxController.java
@@ -147,7 +147,7 @@ public class WxController {
 				+ "</spbill_create_ip>" + "<notify_url>" + notify_url
 				+ "</notify_url>" + "<trade_type>" + trade_type
 				+ "</trade_type>" + "<openid>" + openid + "</openid>"
-				+ "<attach>trade</attach><sub_mch_id>1455666502</sub_mch_id>"
+				+ "<attach>clear</attach>"
 				+ "</xml>";
 		System.out.println("xml=" + xml);
 		String createOrderURL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
