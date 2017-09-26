0cdc3096f3259e8391297acf632c2dcd171968e7
14768776836 <784700109@qq.com>
2017-09-26 13:58:56 +0800

    微信支付controller

--- src/main/java/com/bradypod/web/handler/mobileter/wx/WxController.java
+++ src/main/java/com/bradypod/web/handler/mobileter/wx/WxController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.bradypod.util.wx.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,62 +30,15 @@ import java.util.TreeMap;
 @Controller
 @RequestMapping(value = "/wxController")
 public class WxController {
-	/**
-	 *
-	 *
-	 * 类描述：根据code获取用户信息 <br>
-	 * 作者：田帅 <br>
-	 * 创建时间：2017-09-16 <br>
-	 * 版本：V1.0
-	 */
-	@RequestMapping(value = "/wxLogin")
-	public Object wxLogin(String code) {
-		Map<String, Object> json = new HashMap<String, Object>();
-		/** 请求结果 */
-		String result = WxUserInfo.getWxUserInfo(code);
-		JSONObject jsonObject = (JSONObject) JSON.parse(result);
-		// 微信openid
-		String openid = jsonObject.getString("openid");
-		String nickname = jsonObject.getString("nickname");
-		String headimgurl = jsonObject.getString("headimgurl");
-		System.out.println("openid=" + openid);
-		System.out.println("nickname=" + nickname);
-		System.out.println("headimgurl=" + headimgurl);
-
-		json.put("userId", openid);
-		json.put("nickname", nickname);
-		json.put("headimgurl", headimgurl);
-		json.put("success", true);
-		return json;
-	}
 
 	/**
-	 * 
-	 * 方法描述: 微信绑定平台的用户手机号<br>
-	 * 作者：田帅 <br>
-	 * 创建时间：2017-09-16 <br>
-	 * 版本：V1.0
-	 */
-	@RequestMapping(value = "/wxBound")
-	@ResponseBody
-	public Object wxBound(String mobile, String openid, String nickname,String headimgurl) {
-		Map<String, Object> json = new HashMap<String, Object>();
-		System.out.println("mobile=" + mobile);
-		System.out.println("openid=" + openid);
-		System.out.println("nickname=" + nickname);
-		System.out.println("headimgurl=" + headimgurl);
-		if (mobile == null || mobile.isEmpty() || openid == null
-				|| openid.isEmpty()) {
-			json.put("status", "203");
-			json.put("message", "参数不完整");
-			return json;
-		}
-		//赋值书手机号
-			/* 根据手机号查询账号是否存在 */
-			// 该手机号还没有注册
-			// 该手机号已注册
-			// 先查询该手机号是否已绑定微信openid
-		return json;
+	 * 跳转微信
+	 * @return
+     */
+	@RequestMapping(value = "/wxLoginHtml")
+	public String wxMain(ModelMap map){
+		map.addAttribute("userName","123");
+		return "/apps/business/platform/game/wxGetCode/main";
 	}
 
 	/**
